5a475d2dbb3fad382e98ab78116d6e0ee83fa492
torusrxxx <torusrxxx@users.noreply.github.com>
2017-03-29 17:14:29 +0800

    日志前面加上地址

--- PluginTemplate/plugin.cpp
+++ PluginTemplate/plugin.cpp
@@ -1,40 +1,5 @@
 #include "plugin.h"
 
-enum
-{
-    MENU_TEST,
-    MENU_DISASM_ADLER32,
-    MENU_DUMP_ADLER32,
-    MENU_STACK_ADLER32
-};
-
-static void Adler32Menu(int hWindow)
-{
-    if(!DbgIsDebugging())
-    {
-        _plugin_logputs("[" PLUGIN_NAME "] You need to be debugging to use this command");
-        return;
-    }
-    SELECTIONDATA sel;
-    GuiSelectionGet(hWindow, &sel);
-    duint len = sel.end - sel.start + 1;
-    unsigned char* data = new unsigned char[len];
-    if(DbgMemRead(sel.start, data, len))
-    {
-        DWORD a = 1, b = 0;
-        for(duint index = 0; index < len; ++index)
-        {
-            a = (a + data[index]) % 65521;
-            b = (b + a) % 65521;
-        }
-        delete[] data;
-        DWORD checksum = (b << 16) | a;
-        _plugin_logprintf("[" PLUGIN_NAME "] Adler32 of %p[%X] is: %08X\n", sel.start, len, checksum);
-    }
-    else
-        _plugin_logputs("[" PLUGIN_NAME "] DbgMemRead failed...");
-}
-
 static bool cbLogA(int argc, char* argv[])
 {
     if(argc < 2)
@@ -83,7 +48,7 @@ static bool cbLogA(int argc, char* argv[])
         A[size - 1] = 0;
         text = FromLCP(std::string(A));
     }
-    _plugin_logprintf("\"%s\"\n", ToUtf8(text).c_str());
+    _plugin_logprintf("%p: \"%s\"\n", addr, ToUtf8(text).c_str());
     delete []A;
     return true;
 }
@@ -139,35 +104,9 @@ static bool cbLogW(int argc, char* argv[])
         A[size - 1] = 0;
         text = std::wstring(A);
     }
-    _plugin_logprintf("\"%s\"\n", ToUtf8(text).c_str());
+    _plugin_logprintf("%p: L\"%s\"\n", addr, ToUtf8(text).c_str());
     delete[]A;
     return true;
-}
-
-
-PLUG_EXPORT void CBMENUENTRY(CBTYPE cbType, PLUG_CB_MENUENTRY* info)
-{
-    switch(info->hEntry)
-    {
-    case MENU_TEST:
-        MessageBoxA(hwndDlg, "Test Menu Entry Clicked!", PLUGIN_NAME, MB_ICONINFORMATION);
-        break;
-
-    case MENU_DISASM_ADLER32:
-        Adler32Menu(GUI_DISASSEMBLY);
-        break;
-
-    case MENU_DUMP_ADLER32:
-        Adler32Menu(GUI_DUMP);
-        break;
-
-    case MENU_STACK_ADLER32:
-        Adler32Menu(GUI_STACK);
-        break;
-
-    default:
-        break;
-    }
 }
 
 //Initialize your plugin data here.
@@ -182,7 +121,8 @@ bool pluginInit(PLUG_INITSTRUCT* initStruct)
 //Deinitialize your plugin data here (clearing menus optional).
 bool pluginStop()
 {
-    _plugin_unregistercommand(pluginHandle, PLUGIN_NAME);
+    _plugin_unregistercommand(pluginHandle, "loga");
+    _plugin_unregistercommand(pluginHandle, "logw");
     _plugin_menuclear(hMenu);
     _plugin_menuclear(hMenuDisasm);
     _plugin_menuclear(hMenuDump);
@@ -193,8 +133,4 @@ bool pluginStop()
 //Do GUI/Menu related things here.
 void pluginSetup()
 {
-    //_plugin_menuaddentry(hMenu, MENU_TEST, "&Menu Test");
-    //_plugin_menuaddentry(hMenuDisasm, MENU_DISASM_ADLER32, "&Adler32 Selection");
-    //_plugin_menuaddentry(hMenuDump, MENU_DUMP_ADLER32, "&Adler32 Selection");
-    //_plugin_menuaddentry(hMenuStack, MENU_STACK_ADLER32, "&Adler32 Selection");
 }
